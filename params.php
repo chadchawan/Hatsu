4960e9a698e6df743879ccc43f3bcefe87ad27fd
Chadchawan Radcharid <chadchawan.r@gmail.com>
2018-09-04 00:34:25 +0700

    Some more params

--- params.php
+++ params.php
@@ -1,5 +1,18 @@
 <?php
 define('FCC_USER', 'sysdba');
 define('FCC_PWD', 'masterkey');
+define('PARADOX_PWD', '12345678');
+define('FCC_LOCATION', '127.0.0.1//d:gsm2005/database/fcc.gdb');
+define('shiftendmeter', 'shiftendmeters.px');
+define('endofdaytank', 'endofdaytank.px');
+
+# Grade ID must match in FCC
+$grade = array (
+	array('3','ADO'),
+	array('7','G91'),
+	array('11','E20'),
+	array('13','PADO'),
+	array('15','PG95')
+);
 
 ?>
